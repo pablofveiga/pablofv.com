6b112381c1ca93e4fddcf0534d4c4cfd3b90fc52
Pablo <pablofveiga@gmail.com>
2018-12-14 02:02:27 +0100

    whoiam and work experience splitted to mpa version

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,56 +1,76 @@
 $(document).ready(function(){
 
 
-	function hideHeaders() {
-		$("#main-header h2, #main-header hr, #main-header h1").addClass("hidden");		
-	}
-	setTimeout(hideHeaders,2800);
+	// function hideHeaders() {
+	// 	$("#main-header h2, #main-header hr, #main-header h1").addClass("hidden");		
+	// }
+	// setTimeout(hideHeaders,2800);
 
 	function flipNinja() {	
 		$(".ninja:not(.pos01)").click(function(){
 
-			// $(".main-header").toggleClass("animated01");
-			$(".main-header").addClass("animated01");
+			// $(".mainHeader").toggleClass("animated01");
+			$(".mainHeader").addClass("animated01");
 			$(".ninja").addClass("pos01");
 		})
 		$("h3").click(function(){
-			$(".main-header").addClass("reverse");
+			$(".mainHeader").addClass("reverse");
 			console.log("cacaa");
 		});
 
-		$(".ninja.pos01").click(function(){
-			$(".main-header").addClass("reverse");
-			// console.log("cacaa");
-			// alert("cacaa");
+		// $(".ninja.pos01").click(function(){
+		// 	$(".mainHeader").addClass("reverse");
+		// 	 	console.log("cacaa");
+		// 	 	alert("cacaa");
+		// })
+
+		var content = $('.mainHeader');
+
+		// var trigger = $(".ninja").live('click', function() {
+		// 	content.removeClass('reverse').addClass('running');
+		// }, function() {
+		// 	content.removeClass('running').addClass('reverse');
+		// }).trigger('click');
+
+
+		$(".ninja").click(function(){			
+
+			if( !($(".mainHeader").hasClass("running") || $(".mainHeader").hasClass("reverse"))   ) {
+				$(".mainHeader").addClass("running");
+				console.log("nerea")
+
+			} else {
+				console.log("Caca333")
+				$('.mainHeader').toggleClass("running reverse");
+			}
+
+			// content.removeClass('reverse').addClass('running');
+			// content.removeClass('running').addClass('reverse');
 		})
 
-		var content = $('.main-header');
+		// $(".lobby__item").mouseenter(function(){
+		// 	console.log ("tetsas")
+		// 	$(".mrPacMan").removeClass("hungry");
+		// });
+		
+		// $(".lobby__item").mouseleave(function(){
+		// 	console.log ("tetsas")
+		// 	$(".mrPacMan").addClass("hungry").fadeOut(600);
+		// });
 
-		var trigger = $(".ninja").live('click', function() {
-			content.removeClass('reverse').addClass('running');
-		}, function() {
-			content.removeClass('running').addClass('reverse');
-		}).trigger('click');
+		// $('#trigger').live('click', function() {
+		// 	$(this).toggle(function() {
+		// 		$(this).text('hide follow!');
+		// 		content.removeClass('reverse').addClass('running');
+		// 	}, function() {
+		// 		$(this).text('follow me!');
+		// 		content.removeClass('running').addClass('reverse');
+		// 	}).trigger('click');
+		// });
 
-    $(".lobby__item").mouseenter(function(){
-    	console.log ("tetsas")
-    	$(".mrPacMan").removeClass("hungry");
-    });
-	
-    $(".lobby__item").mouseleave(function(){
-    	console.log ("tetsas")
-    	$(".mrPacMan").addClass("hungry").fadeOut(600);
-    });
-
-    $('#trigger').live('click', function() {
-        $(this).toggle(function() {
-            $(this).text('hide follow!');
-            content.removeClass('reverse').addClass('running');
-        }, function() {
-             $(this).text('follow me!');
-            content.removeClass('running').addClass('reverse');
-        }).trigger('click');
-    });
+		// $("#trigger").click(function(){
+		// 	content.toggleClass("reverse running");
+		// })
 
 	}
 
