4ac080b4be9e6be1366e6c3af352a2b4e691d001
pablofv <pablofveiga@gmail.com>
2019-08-22 15:18:03 +0200

    work experience works

--- dist/js/filtertools.js
+++ dist/js/filtertools.js
@@ -1,19 +1,23 @@
 
     // sticky filter bar for jobs
-    var selectBar = $('.jobSelector').offset().top;   	
-   	var stickySelect = function(){
-	    var scrollTop = $(window).scrollTop();
-	    if (scrollTop > selectBar) { 
-	        $('.jobSelector').addClass('sticky');
-	    } else {
-	        $('.jobSelector').removeClass('sticky'); 
-	    }
-	};
-	stickySelect();
-	$(window).scroll(function() {
-		stickySelect();
-	});
+ //    var selectBar = $('.jobSelector').offset().top;   	
+ //   	var stickySelect = function(){
+	//     var scrollTop = $(window).scrollTop();
+	//     if (scrollTop > selectBar) { 
+	//         $('.jobSelector').addClass('sticky');
+	//     } else {
+	//         $('.jobSelector').removeClass('sticky'); 
+	//     }
+	// };
+	// stickySelect();
+	// $(window).scroll(function() {
+	// 	stickySelect();
+	// });
 
+    $("#pin").click(function(e){
+        e.preventDefault();
+        $('.jobSelector').toggleClass('sticky'); 
+    })
 
 
     $("#collapse").click(function(e){
@@ -61,16 +65,20 @@
         $(".jobsContainer").toggleClass("asc desc");
     });
 
+
+
     // SCROLLED
         // Add event listener on click and delegate over the classes (change that id thing)
     $("#scrollV").click(function(e){
         e.preventDefault();   
         
-        // let flag = "active";
+        // // let flag = "active";
 
         if( $(".scrollH").hasClass("active")  ) {
             console.log("el otro está activo");
             $(".scrollH").removeClass("active");
+            $('.jobsContainer').slick("destroy")
+
         } else {
             console.log("no habia nada apretao");
         }
@@ -78,24 +86,39 @@
         $(this).toggleClass("active");
         $(".jobsContainer").toggleClass("scrolledV");
     });
+
+
+
+
+
     $("#scrollH").click(function(e){
         e.preventDefault();
 
-        if( $(".scrollV").hasClass("active")  ) {
-            console.log("el otro está activo");
-            $(".scrollV").removeClass("active");
-        } else {
-            console.log("no habia nada apretao");
-        }
+        // if( $(".scrollV").hasClass("active")  ) {
+        //     console.log("el otro está activo");
+        //     $(".scrollV").removeClass("active");
+        // } else {
+        //     console.log("no habia nada apretao");
+        // }
 
-        $(this).toggleClass("active");
-        $(".jobsContainer").toggleClass("scrolledH");
+        // $(this).toggleClass("active");
+        // $(".jobsContainer").toggleClass("scrolledH");
 
-        // scrolling mouse wheel horizontal
-        $(".jobsContainer.scrolledH").mousewheel(function(event, delta) {
-            this.scrollLeft -= (delta * 30);
-            event.preventDefault();
-        });
+        // // scrolling mouse wheel horizontal
+        // $(".jobsContainer.scrolledH").mousewheel(function(event, delta) {
+        //     this.scrollLeft -= (delta * 30);
+        //     event.preventDefault();
+        // });
+
+            if ($(this).hasClass("active")) {
+                $(this).removeClass("active")
+                $('.jobsContainer').slick("destroy")
+            } else {
+                $(this).addClass("active")
+                $('.jobsContainer').slick({
+                    dots: true
+                }); 
+            }
 
     });
 
