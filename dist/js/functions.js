dc2e6e0be5fa9a455e803fcfabfc468e6abfb50b
pablofv <pablofveiga@gmail.com>
2019-08-26 20:50:45 +0200

    starting to quit garbage, some links fixed

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -365,45 +365,48 @@ $(".skillsList li").dblclick(function(){
 
 
 // FORMULARIO
+$(".boton_envio").click(function(e) {
+    e.preventDefault();
 
-    $(".boton_envio").click(function(e) {
-        e.preventDefault();
- 
-        var nombre = $(".nombre").val();
-            email = $(".email").val();
-            validacion_email = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
-            mensaje = $(".mensaje").val();
- 
-        if (nombre == "") {
-            $(".nombre").focus();
-            console.log("error nombre");
-            return false;
-        }else if(email == "" || !validacion_email.test(email)){
-            $(".email").focus();    
-            console.log("error mail");
-            return false;
-        }else if(mensaje == ""){
-            $(".mensaje").focus();
-            console.log("error mensaje");
-            return false;
-        }else{
-            $('.ajaxgif').removeClass('hide');
-            var datos = 'nombre='+ nombre + '&email=' + email + '&mensaje=' + mensaje;
-            $.ajax({
-                type: "POST",
-                url: "proceso.php",
-                data: datos,
-                success: function() {
-                    $('.ajaxgif').hide();
-                    $('.msg').text('Message sent!').addClass('msg_ok').fadeOut(5000);  
-                },
-                error: function() {
-                    $('.ajaxgif').hide();
-                    $('.msg').text('Something went wrong...').addClass('msg_error').fadeOut(5000);
-                }
-            });
-            document.getElementById("form").reset();
-            return false;
-        }
- 
-    });
+    var nombre = $(".nombre").val();
+        email = $(".email").val();
+        validacion_email = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+        mensaje = $(".mensaje").val();
+
+    if (nombre == "") {
+        $(".nombre").focus();
+        $(".nombre").parent().prepend("<span class='inputError'>Please type a valid name</span>");
+        $(".inputError").fadeOut(3000);        
+        return false;
+    }else if(email == "" || !validacion_email.test(email)){
+        $(".email").focus();    
+        $(".email").parent().prepend("<span class='inputError'>Please type a valid email</span>");
+        $(".inputError").fadeOut(3000);
+        return false;
+    }else if(mensaje == ""){
+        $(".mensaje").focus();        
+        $(".mensaje").parent().prepend("<span class='inputError msgError'>Please write something</span>");
+        $(".inputError").fadeOut(3000);
+        return false;
+    }else{
+        $('.ajaxgif').removeClass('hide');
+        var datos = 'nombre='+ nombre + '&email=' + email + '&mensaje=' + mensaje;
+        $.ajax({
+            type: "POST",
+            url: "proceso.php",
+            data: datos,
+            success: function() {
+                $('.ajaxgif').hide();
+                $('.msg').text('Message sent!').addClass('msg_ok').fadeOut(5000);  
+                $(".sentMsg").addClass("visible");
+            },
+            error: function() {
+                $('.ajaxgif').hide();
+                $('.msg').text('Something went wrong...').addClass('msg_error').fadeOut(5000);
+            }
+        });
+        document.getElementById("form").reset();
+        return false;
+    }
+
+});
