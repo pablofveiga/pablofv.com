ce74bb758ccb371468c3f1306dd28de31950db0d
pablofv <pablofveiga@gmail.com>
2019-08-26 18:45:02 +0200

    contac form working

--- src/js/functions.js
+++ src/js/functions.js
@@ -361,3 +361,49 @@ $(".inConstruction").fadeOut(10000);
 $(".skillsList li").dblclick(function(){
 	$(this).toggleClass("clicked");
 })
+
+
+
+// FORMULARIO
+
+    $(".boton_envio").click(function(e) {
+        e.preventDefault();
+ 
+        var nombre = $(".nombre").val();
+            email = $(".email").val();
+            validacion_email = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+            mensaje = $(".mensaje").val();
+ 
+        if (nombre == "") {
+            $(".nombre").focus();
+            console.log("error nombre");
+            return false;
+        }else if(email == "" || !validacion_email.test(email)){
+            $(".email").focus();    
+            console.log("error mail");
+            return false;
+        }else if(mensaje == ""){
+            $(".mensaje").focus();
+            console.log("error mensaje");
+            return false;
+        }else{
+            $('.ajaxgif').removeClass('hide');
+            var datos = 'nombre='+ nombre + '&email=' + email + '&mensaje=' + mensaje;
+            $.ajax({
+                type: "POST",
+                url: "proceso.php",
+                data: datos,
+                success: function() {
+                    $('.ajaxgif').hide();
+                    $('.msg').text('Message sent!').addClass('msg_ok').fadeOut(5000);  
+                },
+                error: function() {
+                    $('.ajaxgif').hide();
+                    $('.msg').text('Something went wrong...').addClass('msg_error').fadeOut(5000);
+                }
+            });
+            document.getElementById("form").reset();
+            return false;
+        }
+ 
+    });
