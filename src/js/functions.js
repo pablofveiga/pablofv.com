68bfe0c5f77c8922d080532b39308a4bb0fc015b
pablofv <pablofveiga@gmail.com>
2019-09-19 19:06:53 +0200

    translations ready and linked in subdomain

--- src/js/functions.js
+++ src/js/functions.js
@@ -136,26 +136,34 @@ $(document).ready(function(){
 	    var _id = $(this).attr("data-link");
 	    // var target = document.querySelector(_id)
 	    var target = $(_id);
-	    // Does a scroll target exist?
-	    if (target.length) {
-	        // Only prevent default if animation is actually gonna happen
-	        event.preventDefault();
-	        $('html, body').animate({
-	        	// quit 70 from the sticky header (create filter)
-	            // scrollTop: (target.offset().top - 70)
-	            scrollTop: (target.offset().top)
-	        }, 1000, function() {
-	            // Callback after animation
-	            // Must change focus!
-	            var $target = $(target);
-	            $target.focus();
-	            if ($target.is(":focus")) { // Checking if the target was focused
-	                return false;
-	            } else {
-	                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-	                $target.focus(); // Set focus again
-	            };
-	        });
+	    if (_id == "spanish") {
+	    	window.location="http://es.pablofv.com";
+	    	console.log("cacsa");
+	    } else if (_id == "english") {
+	    	window.location="http://pablofv.com";
+	    	console.log("eeeecacsa");
+	    } else {
+		    // Does a scroll target exist?
+		    if (target.length) {
+		        // Only prevent default if animation is actually gonna happen
+		        event.preventDefault();
+		        $('html, body').animate({
+		        	// quit 70 from the sticky header (create filter)
+		            // scrollTop: (target.offset().top - 70)
+		            scrollTop: (target.offset().top)
+		        }, 1000, function() {
+		            // Callback after animation
+		            // Must change focus!
+		            var $target = $(target);
+		            $target.focus();
+		            if ($target.is(":focus")) { // Checking if the target was focused
+		                return false;
+		            } else {
+		                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+		                $target.focus(); // Set focus again
+		            };
+		        });
+		    }
 	    }
 	});
 // }
